06016eec8240ae3f2f2824849757e78ddff96075
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-08-17 14:40:41 -0700

    added scroll listener to show drum machine

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -5,39 +5,82 @@ import dynamic from 'next/dynamic';
 const Sketch = dynamic(import('./components/Sketch'), {ssr: false})
 import Modal from './components/Modal'
 import Animation from './components/Animation'
-import Introduction from './components/Introduction'
+const Introduction = dynamic(import('./components/Introduction'), {ssr: false});
 const DJ808 = dynamic(import('./components/DJ808'), {ssr: false});
 import Background from './components/Background';
 import styles from './styles/home'
+import { primary } from '../../hocs/Page/styles/colors';
+import Waypoint from 'react-waypoint'
+import SmoothScroll from '../../utils/SmoothScroll';
+import throttle from 'lodash.throttle';
+
 
 class Home extends Component {
   state = {
-    active: true
+    introModal: true,
+    drumVisible: false
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', throttle(e => {
+      if(!this.state.introModal){
+        if(window.scrollY > 0 && this.state.drumVisible){
+          this.setState({drumVisible: false})
+        }else if(window.scrollY == 0 && !this.state.drumVisible){
+          this.setState({drumVisible: true})
+        }
+      }
+    }, 500), {passive: true})
   }
 
   render(){
+    const { introModal, drumVisible } = this.state;
     return (
       <section id="home">
         {/* Intro Modal */}
-        <Modal active={this.state.active}>
+        <Modal active={this.state.introModal}>
           <div className="align-center full-height full-width">
-            <Animation onComplete={() => this.setState({active: false})} />
+            <Animation onComplete={() => introModal && this.setState({introModal: false, drumVisible: true})} />
           </div>
         </Modal>
 
         {/* Drum Machine */}
-        <DJ808 active={!this.state.active} />
+        <DJ808 active={drumVisible} />
 
         {/* Background */}
         <Background />
         
-        {/* Body */}
-        <div className="container">
-          <div className="flex-column full-height">
-            <Introduction />
-            {/* <Sketch /> */}
+        {/* Introduction */}
+        <Introduction  />
+
+        {/* About */}
+        <Waypoint>
+          <div className="container">
+            <div className="flex-column full-height">
+              {/* <About /> */}
+            </div>
           </div>
-        </div>
+        </Waypoint>
+
+        {/* Featured Works */}
+        <Waypoint onEnter={() => console.log('featured-works')}>
+          <div className="container">
+            <div className="flex-column full-height">
+              {/* <About /> */}
+            </div>
+          </div>
+        </Waypoint>
+
+        {/* Sounds */}
+        <Waypoint onEnter={() => console.log('sounds')}>
+          <div className="container">
+            <div className="flex-column full-height">
+              {/* <About /> */}
+            </div>
+          </div>
+        </Waypoint>
+
+
         <style jsx>{styles}</style>
       </section>
     )
