df7aff2e02642de36483bd880ccb79150032cab3
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-08-18 00:13:58 -0700

    added new sections to home page

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -1,24 +1,35 @@
 import React, {Component} from 'react'
-import Link from 'next/link'
-import PageWrapper from '../../hocs/Page'
+
+// Utils
 import dynamic from 'next/dynamic';
+import throttle from 'lodash.throttle';
+
+// HOCS
+import PageWrapper from '../../hocs/Page'
+
+// Components
 const Sketch = dynamic(import('./components/Sketch'), {ssr: false})
 import Modal from './components/Modal'
 import Animation from './components/Animation'
-const Introduction = dynamic(import('./components/Introduction'), {ssr: false});
-const DJ808 = dynamic(import('./components/DJ808'), {ssr: false});
-import Background from './components/Background';
-import styles from './styles/home'
-import { primary } from '../../hocs/Page/styles/colors';
 import Waypoint from 'react-waypoint'
 import SmoothScroll from '../../utils/SmoothScroll';
-import throttle from 'lodash.throttle';
+
+// Containers
+const DJ808 = dynamic(import('./components/DJ808'), {ssr: false});
+const Introduction = dynamic(import('./components/Introduction'), {ssr: false});
+const About = dynamic(import('../About'), {ssr: false})
+const Works = dynamic(import('../Works'), {ssr: false})
+import Background from './components/Background';
+
+// CSS
+import styles from './styles/home'
+import { primary } from '../../hocs/Page/styles/colors';
 
 
 class Home extends Component {
   state = {
-    introModal: true,
-    drumVisible: false
+    introModal: false,
+    drumVisible: true
   }
 
   componentDidMount(){
@@ -36,7 +47,7 @@ class Home extends Component {
   render(){
     const { introModal, drumVisible } = this.state;
     return (
-      <section id="home">
+      <div id="page-content">
         {/* Intro Modal */}
         <Modal active={this.state.introModal}>
           <div className="align-center full-height full-width">
@@ -44,45 +55,23 @@ class Home extends Component {
           </div>
         </Modal>
 
-        {/* Drum Machine */}
-        <DJ808 active={drumVisible} />
-
         {/* Background */}
         <Background />
+
+        {/* Drum Machine */}
+        <DJ808 active={drumVisible} />
         
         {/* Introduction */}
-        <Introduction  />
+        <Introduction onEnter={() => console.log('intro')}/>
 
         {/* About */}
-        <Waypoint>
-          <div className="container">
-            <div className="flex-column full-height">
-              {/* <About /> */}
-            </div>
-          </div>
-        </Waypoint>
+        <About onEnter={() => console.log('about')} />
 
         {/* Featured Works */}
-        <Waypoint onEnter={() => console.log('featured-works')}>
-          <div className="container">
-            <div className="flex-column full-height">
-              {/* <About /> */}
-            </div>
-          </div>
-        </Waypoint>
-
-        {/* Sounds */}
-        <Waypoint onEnter={() => console.log('sounds')}>
-          <div className="container">
-            <div className="flex-column full-height">
-              {/* <About /> */}
-            </div>
-          </div>
-        </Waypoint>
-
+        <Works onEnter={() => console.log('works')} />
 
         <style jsx>{styles}</style>
-      </section>
+      </div>
     )
   }
 }
