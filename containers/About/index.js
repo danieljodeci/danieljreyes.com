ef31b279cf19ae42d15b2e077044c9b41ccc5cee
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-08-18 00:10:13 -0700

    created About section

--- containers/About/index.js
+++ containers/About/index.js
@@ -1,29 +1,51 @@
 import React, {Component} from 'react'
-import Link from 'next/link'
-import PageWrapper from '../../hocs/Page'
+import Waypoint from 'react-waypoint'
+import styles, {globalStyles} from './styles/about'
+import Parallax from 'react-rellax';
+import WiggleWorm from '../Home/components/Introduction/WiggleWorm';
+import { tertiary, primary, secondary } from '../../hocs/Page/styles/colors';
 
-class About extends Component {
+export default class About extends Component {
   render(){
     return (
-      <div>
-        <Link href="/"><a>home</a></Link>
-      </div>
+      <Waypoint onEnter={this.props.onEnter} onLeave={this.props.onLeave}>
+        <section id="about">
+
+          {/* Skills */}
+          <Parallax className="skills" speed={5}>
+            <h4>Skills</h4>
+            <ul>
+              <li><span>Front-End Dev</span></li>
+              <li><span>Back-End Dev</span></li>
+              <li><span>CAD Design</span></li>
+              <li><span>Hardware Programming</span></li>
+              <li><span>Music Composition</span></li>
+            </ul>
+          </Parallax>
+          
+          {/* About Paragram */}
+          <div className="section-container">
+            {/* More wiggle worms */}
+            <WiggleWorm size={60} left={350} top={350} color={secondary.light} speed={-2} />
+            <WiggleWorm size={80} top={50} right={350} color={primary.light} speed={2} />
+
+            {/* Bio */}
+            <div className="section-row">
+              <div className="bio">
+                <h2>Ab_<br />out<strong>:</strong></h2>
+                <p>I am an artist, software developer, entrepeneur, and musician residing in Los Angeles, CA. I am a graduate of CalArts and a founder of BudSpot. I spend my days dwelling upon how I can create things that will make tomorrow feel a bit more comfortable. I love synthesizers and kinetic sound sculptures.</p>
+              </div>
+            </div>
+
+          </div>
+          <style jsx>{styles}</style>
+        </section>
+      </Waypoint>
     )
   }
 }
 
-export default PageWrapper(About, {
-  metaData: {
-    title: 'About - DANIEL JODECI',
-    link: [],
-    meta: [
-      { name: 'About - DANIEL JODECI', content: 'Portfolio Site' },
-      { property: 'og:image:type', content: 'image/png' },
-      { property: 'og:image:width', content: '640' },
-      { property: 'og:image:height', content: '640' },
-      { property: "og:title", content: 'About - DANIEL JODECI' },
-      { property: "og:description", content: 'Portfolio Site' },
-      { property: "og:type", content: "website" },
-    ]
-  },
-})
+About.defaultProps ={
+  onEnter: () => null,
+  onLeave: () => null
+}
---
+++ containers/About/styles/about.js
@@ -0,0 +1,41 @@
+import css, {global} from 'styled-jsx/css'
+import { primary, tertiary, secondary } from '../../../hocs/Page/styles/colors';
+
+export default css`
+  section#about {
+    padding-top: 60px;
+    height: 100vh;
+    width: 100%;
+    position: relative;
+    z-index: 1;
+  }
+
+  .bio {
+    width: 50%;
+    position: relative;
+  }
+
+  .bio h2 {
+    margin: 0;
+    font-size: 120px;
+    line-height: 150px;
+    font-weight: 700;
+    margin-bottom: 75px;
+    margin-left: -7px;
+    color: ${tertiary.neutral};
+  }
+
+  .bio strong {
+    color: ${secondary.light};
+  }
+
+  .bio p {
+    margin: 0;
+    font-family: "Futura", monospace;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 38px;
+    transition: color .4s ease;
+    color: ${tertiary.dark};
+  }
+`
