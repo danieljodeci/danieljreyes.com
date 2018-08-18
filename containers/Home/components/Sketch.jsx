44d2e85c081eedcab0893d8e4c5a21719dc929e5
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-08-18 00:12:24 -0700

    added Wrapper to Sketch

--- containers/Home/components/Sketch.jsx
+++ containers/Home/components/Sketch.jsx
@@ -1,3 +1,4 @@
+import React, {Component} from 'react'
 import P5Wrapper from '../../../hocs/P5Wrapper'
 import {primary} from '../../../hocs/Page/styles/colors';
 
@@ -6,14 +7,14 @@ const sketch = p => {
   const SPACING = 2;
 
   p.preload = function() {
-    img_scale = p.loadImage('http://www.homewebprofits.net/wp-content/uploads/2018/02/off-white-color-intended-for-off-white-color-palette.png');
+    img_scale = p.loadImage('/static/images/rainbow-mountains.jpg');
   }
 
   p.setup = function() {
     const canvas = p.createCanvas(800, 800);
     p.noStroke();
     scale_factor = 8;
-    img_scale.resize(600 / scale_factor, 600 / scale_factor);
+    img_scale.resize(800 / scale_factor, 800 / scale_factor);
     img_scale.loadPixels();
     particles = [];
 
@@ -32,7 +33,7 @@ const sketch = p => {
     img_scale.loadPixels();
 
     
-    p.translate(100, 100);
+    // p.translate(100, 100);
 
     // p.ellipse((p.sin(p.frameCount * 0.02) * 100) + 300, (p.cos(p.frameCount * 0.02) * 100) + 200, 5, 5);
 
@@ -43,8 +44,8 @@ const sketch = p => {
         let limit = 1;
         let scale = 0.8
         let repeller = p.createVector(
-          (p.sin(p.frameCount * 0.02) * 100) + 300,
-          (p.cos(p.frameCount * 0.02) * 100) + 300);
+          (p.sin(p.frameCount * 0.2) * 300) * p.cos(p.frameCount * 0.09) + 400,
+          (p.tan(p.frameCount * 0.2) * 300) * p.sin(p.frameCount * 0.09) + 400);
 
         let attr = p.createVector(
           (i * scale_factor),
@@ -52,12 +53,12 @@ const sketch = p => {
         );
 
         particles[index].attract(attr, {
-          acc_scale: 0.1,
+          acc_scale: 0.4,
           vel_limit: 1
         });
 
         particles[index].repel(repeller, {
-          acc_scale: 0.1,
+          acc_scale: 0.4,
           vel_limit: 1
         });
 
@@ -128,4 +129,15 @@ let Particle = function(renderer, loc, params) {
   };
 }
 
-export default P5Wrapper()(sketch)
+const Sketch = P5Wrapper()(sketch)
+
+
+export default class Wrapper extends Component {
+  render(){
+    return (
+      <section id="sketch" className="align-center full-width">
+        <Sketch />
+      </section>
+    )
+  }
+}
