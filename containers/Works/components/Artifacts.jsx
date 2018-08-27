84a700e77f16f8b5e6742654fd2dda953f8959db
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-08-26 22:52:40 -0700

    added artifacts description

--- containers/Works/components/Artifacts.jsx
+++ containers/Works/components/Artifacts.jsx
@@ -18,9 +18,30 @@ export default class Artifacts extends Component {
       <Fragment>
         <Modal active={this.state.active} onToggleModal={() => this.setState({active: !this.state.active})}>
           <div className="section-container">
-            <div className="section-row">
-              <p>Artifacts</p>
+            <div className="full-width full-height align-center">
+              <div className="flex-row project-body">
+
+                {/* Project Media */}
+                <div className="project-media">
+                  <img src="/static/images/artifacts.png" />
+                </div>
+
+                {/* Description */}
+                <div className="project-description">
+                  <h3>Artifacts</h3>
+                  <p>Exposing the artifacts in one’s self image through an array of iPhone and iPad cameras. Each successive phone captures its’ neighboring screen, amplifying the distortion of signals until one’s self image becomes indistinguishable; an aliasing selfie.</p>
+                  <p>Tech used to complete this project: </p>
+                  <ul>
+                    <li><p>Solidworks</p></li>
+                    <li><p>iPhone 5</p></li>
+                    <li><p>iPhone 6</p></li>
+                    <li><p>iPad</p></li>
+                    <li><p>Taz 5 3D Printer</p></li>
+                  </ul>
+                </div>
+              </div>
             </div>
+            <style jsx>{styles}</style>
           </div>
         </Modal>
         <Parallax className="project-section" speed={-1}>
