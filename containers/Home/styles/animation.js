ed783d2704c9d58c3b68d0afc2362b62f8ce6f8b
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-08-27 13:43:22 -0700

    changed intro animation

--- containers/Home/styles/animation.js
+++ containers/Home/styles/animation.js
@@ -4,7 +4,7 @@ export default css`
   .path {
     fill: rgba(25, 200, 35, 0);
     stroke: url(#MyGradient);
-    stroke-width: 5;
+    stroke-width: 1;
     stroke-linecap: round;
     stroke-miterlimit: 2;
   }
