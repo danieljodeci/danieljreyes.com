7570dc3ad4bdeffc84be8fd9f8b3fd2eeccb4758
Jodeci Reyes <danieljodecireyes@gmail.com>
2018-09-07 13:29:27 -0700

    set a max height for works intro to keep budspot on the right

--- containers/Works/components/Intro.jsx
+++ containers/Works/components/Intro.jsx
@@ -6,7 +6,7 @@ export default class Intro extends Component {
   render(){
     const { mobile, tablet } = this.props
     return (
-      <Parallax className="project-section" speed={mobile || tablet ? 0 : 7}>
+      <Parallax className="project-section" style={{maxHeight: 500}} speed={mobile || tablet ? 0 : 7}>
         <p className="works-intro">I started my exploration into technology as a musician, attempting to recreate physical phenomena found in sound into experiential environments. With this curiousity, came the need to learn a multitude of tech stacks in order to produce the final output. Familiarizing myself with multiple languages over time enhanced my ability to recognize the similarities between most languages and acquire the skill to adapt to emerging technologies as efficiently as possible. Here you can view some of the selected works that helped me develop these skills.</p>
         <style jsx>{styles}</style>
       </Parallax>
